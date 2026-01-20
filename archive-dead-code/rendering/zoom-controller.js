77e5304e76f59660d228a8b3db1ec9b6fd74c9b3
maddi <adampigeons@gmail.com>
2026-01-20 13:09:41 -0800

    Remove archived dead code folder

--- archive-dead-code/rendering/zoom-controller.js
+++
@@ -1,9 +0,0 @@
-// Control view based on zoom level and hover state
-function updateZoomView(zoom) {
-  // Clear hover state when zooming - let zoom level dictate visibility
-  if (zoom < ZOOM.AD_INFO_THRESHOLD && AppState.adInfoHovered) {
-    AppState.adInfoHovered = false;
-  }
-  
-  updateVisibility();
-}
